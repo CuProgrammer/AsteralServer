ac44c28c0b5ca81c306502d02634565deecc2368
Er Kei <ergrammer@yahoo.com>
2024-02-06 19:51:09 +0330

    Set up main file into a functional state

--- AsteralServer.java
+++ AsteralServer.java
@@ -1,10 +1,27 @@
 package com.asteral.asteralserver;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class AsteralServer {
 
     public static void main(String[] args) {
         UserManager userManager = new UserManager("data.txt", "buffer.txt");
-        
+        userManager.flush();
+        userManager.load();
+        ProductManager productManager = new ProductManager("pdata.txt", "pbuffer.txt");
+        productManager.flush();
+        productManager.load();
+        RequestManager.setUserManager(userManager);
+        RequestManager.setProductManager(productManager);
+        try (ServerSocket serverSocket = new ServerSocket(8080)) {
+            while (true) {
+                RequestManager requestManager = new RequestManager(serverSocket.accept());
+                requestManager.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         /*ProductManager productManager = new ProductManager("komo.txt", "jumo.txt");
         productManager.flush();
         productManager.load();
